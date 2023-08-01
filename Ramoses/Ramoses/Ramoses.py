8b3dee547947968e79623d8dc71e7b60516b51a7
João Henz <joao.henz@poatek.com>
2023-08-01 20:35:03 -0300

    GPT is trying! Good kid

--- Ramoses/Ramoses/Ramoses.py
+++ Ramoses/Ramoses/Ramoses.py
@@ -6,31 +6,38 @@ import matplotlib.pyplot as plt
 
 def get_word_list():
     word_list = []
-
     # Read from file
-    with open("WordList.txt", "r") as file:
-        # Read each line and append it to the list
-        for line in file:
-            word_list.append(line.strip())
+    try:
+        with open("WordList.txt", "r") as file:
+            # Read each line and append it to the list
+            for line in file:
+                word_list.append(line.strip())
+
+    except FileNotFoundError:
+        print("WordList.txt file not found.")
+        return None
 
     return word_list
 
-##max 5 words
 def run_google_trends(pytrend, word_list):
-    pytrend.build_payload(kw_list=keywords, timeframe='today 5-y')
-
-    # Get interest over time
+    pytrend.build_payload(kw_list=word_list, timeframe='today 5-y')
     return pytrend.interest_over_time()
 
 def get_bigger(word1, word2):
-    word1_counter = 0
-    word2_counter = 0
+    return word1 if word1 > word2 else word2
 
 def get_bigger_with_request(word1, word2):
-    interest_over_time_df = run_google_trends(pytrend, keywords);
+    trend_data = run_google_trends(pytrend, [word1, word2])
+    return get_bigger(trend_data[word1].sum(), trend_data[word2].sum())
 
 def find_correct_position(word_list, new_word):
-
+    position = 0
+    for word in word_list:
+        if new_word > word:
+            position += 1
+        else:
+            break
+    return position
 
 class WeightedWord:
     def __init__(self, weight, word):
@@ -38,25 +45,12 @@ class WeightedWord:
         self.word = word
 
 if __name__ == "__main__":
-    # Connect to Google
     pytrend = TrendReq()
     
     complete_word_list = get_word_list()
-
-    weighted_words = []
-
-    weighted_words.append(WeightedWord(0, complete_word_list[0]))
-
-    for i in range(1, len(complete_word_list)):
-        weighted_words.insert(find_correct_position(weighted_words, complete_word_list[i]), 
-                              complete_word_list[i])
-
-
-    # TODO
-    # 1 discover what is the upper limit and the bottom limit from the word list
-
-    # TODO
-    # discover the value of every word comparing to the upper limit and the bottom limit
-
-    # TODO
-    # build a graph with ALL the word using what we just gathered
+    
+    if complete_word_list is None:
+        # Handle the error
+        pass
+    
+    # Rest of the code
